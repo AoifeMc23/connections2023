9807ba7e551b1025672856d7bd485e9ea1b6f417
AoifeMc23 <132588929+AoifeMc23@users.noreply.github.com>
2023-05-18 08:53:43 +0100

    update 2

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
+var size = 30;
+var maxSize = 100;
+var reset = false;
+var original = 30;
+
 function setup() {
   var canvas1 = createCanvas(windowWidth, windowHeight);
   canvas1.position(0, 0);
 
   canvas1.parent("#canvas1");
-
-  colorMode(HSB, 360, 100, 100, 100);
 
   noLoop();
 }
@@ -14,8 +17,11 @@ var mouseOff = document.getElementById("mouse_off");
 
 mouseOff.style.display = "none";
 
+canvas1.style.display = "none";
+
 function mouseTurnOn() {
   if ((mouseOff.style.display = "none")) {
+    canvas1.style.display = "block";
     mouseOn.style.display = "none";
     mouseOff.style.display = "block";
     loop();
@@ -26,27 +32,42 @@ function mouseTurnOff() {
   if ((mouseOff.style.display = "block")) {
     mouseOn.style.display = "block";
     mouseOff.style.display = "none";
+    canvas1.style.display = "none";
     clear();
     noLoop();
   } else {
     mouseOff.style.display = "block";
-    loop();
   }
 }
 
 function draw() {
+  background(000000);
   noStroke();
 
-  drawingContext.shadowBlur = 1000;
-  drawingContext.shadowColor = (207, 7, 99);
-
-  ellipse(mouseX, mouseY, 33, 33);
-
-  fill(226, 141, 180, 4);
-
-  blendMode(OVERLAY);
+  drawingContext.shadowBlur = 25;
+  drawingContext.shadowColor = color(178, 158, 204);
+  fill(178, 158, 204);
+  ellipse(mouseX + 20, mouseY + 20, size, size);
 }
 
+function mousePressed() {
+  if (size <= maxSize && reset === false) {
+    size += 10;
+  }
+
+  if (size >= maxSize) {
+    reset = true;
+  }
+
+  if (reset === true) {
+    size -= 10;
+  }
+
+  if (reset === true && size === original) {
+    reset = false;
+    size += 10;
+  }
+}
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
